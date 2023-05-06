1c5b3371273a9d41ef5e8c9aa7931095d01e558c
DuckMcQuacks <menus.dominik@gmail.com>
2023-05-06 16:08:41 +0200

    Create 2620_Counter.ts

---
+++ LeetCode/2620_Counter.ts
@@ -0,0 +1,18 @@
+// Link to problem
+// https://leetcode.com/problems/counter/description/
+
+function createCounter(n: number): () => number {
+    let count = n-1;
+    return function() {
+        count++
+        return count
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
