00e38a09cf13cdba9dec6226638ac84941cbe087
DuckMcQuacks <menus.dominik@gmail.com>
2023-05-06 16:01:38 +0200

    Create 2667_Create_Hello_World_Function.ts

---
+++ LeetCode/2667_Create_Hello_World_Function.ts
@@ -0,0 +1,13 @@
+// Link to problem
+// https://leetcode.com/problems/create-hello-world-function/description
+
+function createHelloWorld() {
+	return function(...args): string {
+        return "Hello World"
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
